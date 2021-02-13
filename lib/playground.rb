db86ae673d242831d57f55ade0189dea3ef00277
paria mahmoudi <parimahmoudi62@gmail.com>
2021-02-13 18:46:07 +0330

    init

---
+++ lib/playground.rb
@@ -0,0 +1,15 @@
+class Playground
+
+  def initialize(children)
+    @children  = children
+  end
+
+  def empty?
+    @children == 0
+  end
+
+  def mood
+    'boring'
+  end
+
+end
---
+++ spec/playground_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/playground'
+
+describe Playground do
+  context 'when there are no children' do
+    # before do
+    #   @playground  = Playground.new(0)
+    # end
+
+    let(:playground) { Playground.new(0)}
+
+    it 'is quite boring place' do
+      mood  = playground.mood
+      expect(mood).to eq('boring')
+    end
+
+    it 'is empty' do
+      expect(playground).to be_empty
+    end
+  end
+end
